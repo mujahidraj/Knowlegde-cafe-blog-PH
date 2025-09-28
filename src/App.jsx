96064339fecf78d53856847a02b62591e82b8a20
mujahidraj <mujahidraj65@gmail.com>
2025-09-28 17:43:06 +0600

    data fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 import Navbar from './Components/NavBar/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <Navbar></Navbar>
+     <Blogs></Blogs>
     </>
   )
 }
